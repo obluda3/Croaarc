63c09666d088c57ab0d5f15b253b4d8db306d6ca
obluda3 <yoshivert992@gmail.com>
2021-12-23 16:32:15 +0100

    Woops, turns out the format is way easier than I thought

--- Croaarc/DAT.cs
+++ Croaarc/DAT.cs
@@ -24,28 +24,21 @@ namespace Croaarc
                 var fileCount = br.ReadInt32();
 
                 var initKey = br.ReadByte();
-                byte key = initKey;
                 Console.WriteLine($"File {Path.GetFileName(input)}: {fileCount} files");
                 br.BaseStream.Position = offsetToFileTable;
                 for(var i = 0; i < fileCount; i++)
                 {
                     var fileName1 = br.ReadCStringXored(initKey);
                     var fileName2 = br.ReadCStringXored(initKey);
-                    /*
-                    if(fileName1 != fileName2)
-                    {
-                        Console.WriteLine($"Interesting at 0x{br.BaseStream.Position:X} mismatch between filename 1 and 2 {fileName1} vs {fileName2}");
-                    }*/
+
                     var offset = br.ReadInt32Xored(initKey);
                     var size = br.ReadInt64Xored(initKey);
 
-                    var newKey = br.ReadByte(); // hehe, i'm not stupid, i'm not going to xor the data multiple times
-                    key ^= newKey;  // just update the key with every new one, since the xor operation is associative 
-
+                    var fileKey = br.ReadByteXored(initKey);
                     var bkPos = br.BaseStream.Position;
 
                     br.BaseStream.Position = offset;
-                    var data = br.ReadBytesXored((int)size, key);
+                    var data = br.ReadBytesXored((int)size, fileKey);
 
                     var path = outputDir + Path.DirectorySeparatorChar + fileName2;
                     var directoryName = Path.GetDirectoryName(path);
